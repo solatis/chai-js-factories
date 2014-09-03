b2d5fdf2c3463944949429ce99cc602bb1d25a54
Nathan Houle <nathan@nathanhoule.com>
2014-09-03 03:49:46 -0700

    Automatically require js-factories when possible
    Update documentation to match

--- chai-js-factories.js
+++ chai-js-factories.js
@@ -1,26 +1,36 @@
-(function (chaiJsFactories) {
-    // Module systems magic dance.
-    if (typeof require === "function" && typeof exports === "object" && typeof module === "object") {
-        // NodeJS
-        module.exports = chaiJsFactories;
-    } else if (typeof define === "function" && define.amd) {
-        // AMD
-        define(['js-factories'], function ($) {
-            return function (chai, utils) {
-                return chaiJsFactories(chai, utils, Factory);
-            };
-        });
-    } else {
-        // Other environment (usually <script> tag): plug in to global chai instance directly.
-        chai.use(function (chai, utils) {
-            return chaiJsFactories(chai, utils, Factory);
-        });
+(function(context, factory) {
+  'use strict';
+
+  if (typeof define === 'function' && define.amd) {
+    // AMD
+    define(factory);
+  } else if (typeof exports === 'object') {
+    // Node
+    module.exports = factory.bind(this, require('js-factories'));
+  } else {
+    if (!context.chai) {
+      throw new Error(['Chai could not be found in the current scope. Please ensure Chai is loaded',
+                      'before the chai-js-factories script.'].join(''));
     }
-}(function (chai, utils, jsFactories) {
 
-    jsFactories = jsFactories || Factory;
+    if (typeof context.Factory !== 'object') {
+      throw new Error(['Please include `js-factories` globally as a script, and ensure it is',
+                      'exported as the global `Factory` function.']);
+    }
 
+    // Browser globals
+    context.chai.use(factory.bind(this, Factory));
+  }
+}(this, function(Factory, chai /*, utils */) {
+  'use strict';
 
-    chai.factory = jsFactories;
+  if (typeof Factory !== 'object') {
+    if (typeof define === 'function' && define.amd) {
+      throw new Error('Please pass `js-factories` to the function exported by `chai-js-factories`.');
+    } else {
+      throw new Error('Unknown error: Cannot find the `js-factories` library.');
+    }
+  }
 
+  chai.factory = Factory;
 }));
--- test/chai-js-factories.spec.js
+++ test/chai-js-factories.spec.js
@@ -1,15 +1,15 @@
 if (!chai) {
-    var jsFactories     = require('js-factories');
-    var chai            = require('chai');
-    var chaiJsFactories = require('..');
-    chai.use(chaiJsFactories);
+  var jsFactories     = require('js-factories');
+  var chai            = require('chai');
+  var chaiJsFactories = require('..');
+  chai.use(chaiJsFactories);
 }
 
 describe('chai::factory', function () {
-    it('attaches to chai', function () {
-        chai.assert.property(chai, 'factory');
-        chai.expect(chai.factory).to.respondTo('define');
-        chai.expect(chai.factory).to.respondTo('create');
-        chai.expect(chai.factory).to.respondTo('resetFactories'); 
-    });
-})
+  it('attaches to chai', function () {
+    chai.assert.property(chai, 'factory');
+    chai.expect(chai.factory).to.respondTo('define');
+    chai.expect(chai.factory).to.respondTo('create');
+    chai.expect(chai.factory).to.respondTo('resetFactories');
+  });
+});
